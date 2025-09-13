85e48bca9355f7feb78377631b0a48ee9665e4c1
Akil Mullassery Prashant <94084424+akilmp@users.noreply.github.com>
2025-09-14 00:53:52 +1000

    feat: add auth flow pages

--- web/components/Map.tsx
+++ web/components/Map.tsx
@@ -7,15 +7,14 @@ import { Spot } from '../lib/types';
 export default function Map({ spots }: { spots: Spot[] }) {
   return (
     <MapContainer
-      // @ts-ignore center is valid for MapContainer
+      // @ts-expect-error center is valid for MapContainer
       center={[-33.865143, 151.2099]}
-      // @ts-ignore zoom is valid for MapContainer
       zoom={12}
       className="h-full"
     >
       <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
       {spots.map((spot) => (
-        <Marker key={spot.id} position={[spot.lat, spot.lng] as any}>
+        <Marker key={spot.id} position={[spot.lat, spot.lng] as [number, number]}>
           <Popup>{spot.name}</Popup>
         </Marker>
       ))}
